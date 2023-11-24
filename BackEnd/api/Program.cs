a84211dd77ff9f43ffbf8416d13b5806964350dc
benjasteck <benj5102@easv365.dk>
2023-11-24 16:20:31 +0300

    angular start with files

--- BackEnd/api/Program.cs
+++ BackEnd/api/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.addSpaStaticFiles(conf => conf.RootPAth = "../frontend/www/");
 
 var app = builder.Build();
 
@@ -15,6 +16,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.useSpaStaticFiles();
+app.useSpa(conf =>
+{
+  conf.Options.SourcePath = ".. / frontend / www / ";
+});
 
 app.UseHttpsRedirection();
 
--- BackEnd/infraestructure/DataModels/Order.cs
+++ BackEnd/infraestructure/DataModels/Order.cs
@@ -5,7 +5,7 @@ namespace infraestructure.DataModels;
 
 public class Order
 {
-    public int OrderId { get; set; }
+    public int OrderId { get; set;  }
     public Array OrderItemArrayId { get; set; }
     public DateOnly OrderDate  { get; set; }
     public TimeOnly OrderTime { get; set; }
